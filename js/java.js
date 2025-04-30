f09f8cde28e513fcd7dc28c8361f69d369f6e3e1
Rodrigo Arce C <rodrigoar87@gmail.com>
2025-04-29 20:14:53 -0600

    Update java.js

--- js/java.js
+++ js/java.js
@@ -1,6 +1,6 @@
 function toggleMenu() {
-  var menu = document.getElementById('menu');
-  menu.classList.toggle('active');
+  var menu = document.getElementById("menu");
+  menu.classList.toggle("active");
 }
 function saludar() {
   alert('¡Hola desde DHB Robótica!');
